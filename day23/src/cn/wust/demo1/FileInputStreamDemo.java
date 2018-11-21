3c22a600682a5ee9a4341a33a916b09716493bc9
Dollis <542763565@qq.com>
2018-11-21 20:44:39 +0800

    test

--- day23/src/cn/wust/demo1/FileInputStreamDemo.java
+++ day23/src/cn/wust/demo1/FileInputStreamDemo.java
@@ -6,8 +6,11 @@ import java.io.IOException;
 public class FileInputStreamDemo {
     public static void main(String[] args) throws IOException {
         FileInputStream fis = new FileInputStream("d:\\a.txt");
-        int i=fis.read();
-        System.out.println(i);
+        int len = 0;
+
+        while ((len = fis.read())!=-1) {
+            System.out.print((char)len);
+        }
         fis.close();
     }
 
