86e1a2c2c22164020855a383ca900b1dc34edd4e
dy <minimady@foxmail.com>
2018-11-21 17:26:44 +0800

    test

--- day23/src/cn/wust/demo1/FileInputStreamDemo.java
+++ day23/src/cn/wust/demo1/FileInputStreamDemo.java
@@ -1,4 +1,14 @@
 package cn.wust.demo1;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class FileInputStreamDemo {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis = new FileInputStream("d:\\a.txt");
+        int i=fis.read();
+        System.out.println(i);
+        fis.close();
+    }
+
 }
