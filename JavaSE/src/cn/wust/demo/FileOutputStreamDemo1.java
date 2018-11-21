637b67a416330b276ffa0089b8a7b89d5786ecc0
dy <minimady@foxmail.com>
2018-11-21 16:46:13 +0800

    1

--- JavaSE/src/cn/wust/demo/FileOutputStreamDemo1.java
+++ JavaSE/src/cn/wust/demo/FileOutputStreamDemo1.java
@@ -1,4 +1,16 @@
 package cn.wust.demo;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class FileOutputStreamDemo1 {
+    public static void main(String[] args) throws IOException {
+        File file = new File("d:\\a.txt");
+        FileOutputStream fos = new FileOutputStream(file,true);
+        fos.write("hello\r\n".getBytes());
+        fos.write("world".getBytes());
+
+        fos.close();
+    }
 }
